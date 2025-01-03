12bc455ad9395e693543518d5ef678c75a4caa67
Camilo Hernandez <camilohreina.dev@gmail.com>
2025-01-03 15:22:23 -0500

    add world coordinates and read json file

--- 1.web_map/index.py
+++ 1.web_map/index.py
@@ -4,12 +4,27 @@ import pandas
 data= pandas.read_csv("Volcanoes.txt")
 lat= list(data["LAT"])
 lon= list(data["LON"])
+elev= list(data["ELEV"])
+
+html = """<h4>Volcano information:</h4>
+Height: %s m
+"""
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return "green"
+    elif 1000 <= elevation < 3000:
+        return "orange"
+    else:
+        return "red"
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="cartodb positron")
 fg= folium.FeatureGroup(name="My Map")
 
-for lt,ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+for lt,ln,el in zip(lat, lon, elev):
+    iframe = folium.IFrame(html=html % str(el), width=200, height=100)
+    fg.add_child(folium.CircleMarker(location=[lt, ln],radius=6, popup=folium.Popup(iframe), fill_color=color_producer(el), color="grey", fill_opacity=0.7))
 
+fg.add_child(folium.GeoJson(data=(open("world.json", "r", encoding="utf-8-sig").read())))
 map.add_child(fg)
 map.save("Map1.html")
