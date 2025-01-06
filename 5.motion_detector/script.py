593b388507d8041fe97d9ef7fe355626b5fe8e93
Camilo Hernandez <camilohreina.dev@gmail.com>
2025-01-06 10:50:14 -0500

    add to csv  motion detector

--- 5.motion_detector/script.py
+++ 5.motion_detector/script.py
@@ -1,14 +1,18 @@
-import cv2, time
+import cv2, time, pandas
+from datetime import datetime
 
 first_frame = None
+status_list = [None, None]
+times = []
 
+df= pandas.DataFrame(columns=["Start", "End"])
 
 video = cv2.VideoCapture(2)
 
 
 while True:
     check, frame = video.read()
-
+    status=0
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
@@ -25,19 +29,35 @@ while True:
     for contour in cnts:
         if cv2.contourArea(contour) < 1000:
             continue
+        status=1
+
         (x, y, w, h) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
 
+    status_list.append(status)
+    if (status_list[-1] == 1 and status_list[-2] == 0):
+        times.append(datetime.now())
+    if (status_list[-1] == 0 and status_list[-2] == 1):
+        times.append(datetime.now())
     cv2.imshow("Capturing", gray)
     cv2.imshow("Delta Frame", delta_frame)
     cv2.imshow("Threshold Frame", thresh_frame)
     cv2.imshow("Color Frame", frame)
 
     key = cv2.waitKey(1)
-    print(gray)
-
     if key == ord('q'):
+        if status == 1:
+            times.append(datetime.now())
         break
 
+print(status_list)
+print(times)
+
+for i in range(0, len(times), 2):
+    if i + 1 < len(times):
+        new_row = pandas.DataFrame({"Start": [times[i]], "End": [times[i + 1]]})
+        df = pandas.concat([df, new_row], ignore_index=True)
+
+df.to_csv("Times.csv")
 video.release()
 cv2.destroyAllWindows()
